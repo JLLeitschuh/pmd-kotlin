120d10efa63ae98b14c8e18ae3a6ac5c0776fb4a
Evgeny Naumenko <Evgeny.Naumenko@jetbrains.com>
2015-07-10 19:41:07 +0300

    setup pmd check infrastructure

---
+++ src/main/kotlin/org/jetbrains/Sample.kt
@@ -0,0 +1,17 @@
+fun main(args: Array<String>) {
+    println("A nice place to start")
+    println("A nice place to start")
+    println("A nice place to start")
+    println("A nice place to start")
+    println("A nice place to start")
+    println("A nice place to start")
+}
+
+fun main1(args: Array<String>) {
+    println("A nice place to start")
+    println("A nice place to start")
+    println("A nice place to start")
+    println("A nice place to start")
+    println("A nice place to start")
+    println("A nice place to start")
+}
