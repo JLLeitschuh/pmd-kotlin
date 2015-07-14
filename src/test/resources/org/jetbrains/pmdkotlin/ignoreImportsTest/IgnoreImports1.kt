78a7c936a47e3668263fb8ed0069018acdddfb9f
Anna Malova <an.forgotenn@gmail.com>
2015-07-14 17:12:18 +0300

    Add ignore imports tests

---
+++ src/test/resources/org/jetbrains/pmdkotlin/ignoreImportsTest/IgnoreImports1.kt
@@ -0,0 +1,14 @@
+import org.apache.commons.io.ByteOrderMark
+import org.fusesource.hawtjni.runtime.Callback
+import com.beust.jcommander.IDefaultProvider
+import com.google.protobuf.AbstractMessageLite
+import org.apache.log4j.Level
+import org.classpath.icedtea.Config
+import org.yaml.snakeyaml.Dumper
+import org.yaml.snakeyaml.util.ArrayStack
+import com.intellij.AbstractBundle
+import com.intellij.psi.ClassTypePointerFactory
+
+fun a(args: Array<String>) {
+    println("Hello world")
+}
